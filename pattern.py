315211149cbaf63e534dda8f6be6e10f2d33868b
Abhay V Ashokan <abhayvashokan@gmail.com>
2020-07-29 20:48:57 +0530

    Reduced LOC

--- pattern.py
+++ pattern.py
@@ -1,15 +1,9 @@
 pattern = input("Enter the pattern: ")
-index = pattern.count('L') + pattern.count('l') + 1
-
+index =  pattern.count('L') + pattern.count('l') + 1
 print(index, end="")
 index, taken = index + 1 if pattern[0] in "mM" else index - 1, [index]
-
 for ch in pattern:
-    if ch in 'mM':
-        while index in taken:
-            index += 1
-    else:
-        while index in taken:
-            index -= 1
+    while index in taken:
+        index += 1 if ch in 'mM' else -1
     print(index, end='')
     taken.append(index)
--- substring.py
+++ substring.py
@@ -1,3 +1,2 @@
-string = input("Enter the string: ")
-substr = input("Enter the search string: ")
+string, substr = input("Enter the string: "), input("Enter the search string: ")
 print("Occurances: ", string.count(substr), "\nPositions: ", ", ".join([str(i+1) for i in range(len(string)) if substr in string[i:i+len(substr)]]))
